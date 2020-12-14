79c3e057c55a1593cb493f2a6f597627625e3217
Leandro <thesquallrock@gmail.com>
2020-12-14 16:42:32 +0100

    pushino

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65
+
+var km, eta, prezzo;
+
+km = parseInt(prompt('Inserire numero chilometri'));
+eta = parseInt(prompt('Inserire età'));
+prezzo = km * 0.21;
+console.log(prezzo);
